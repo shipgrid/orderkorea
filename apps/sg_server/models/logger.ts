cfb4cd79ae6ccce66d2eef953e8a7de19c98128e
joecof <joecofg@gmail.com>
2023-11-30 23:23:10 -0800

    testing logger

--- apps/sg_server/models/logger.ts
+++ apps/sg_server/models/logger.ts
@@ -3,6 +3,16 @@ import { getWinsonTransport } from '@hyperdx/node-opentelemetry';
 
 const MAX_LEVEL: string = 'info';
 
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  verbose: 4,
+  debug: 5,
+  silly: 6
+};
+
 const logger: Logger = winston.createLogger({
   level: MAX_LEVEL,
   format: winston.format.json(),
