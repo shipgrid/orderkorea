ad7772e74a231d02a38f70e4f28d83174cdbcded
joecof <joecofg@gmail.com>
2024-01-06 20:18:13 -0800

    update

--- apps/sg-server/index.ts
+++ apps/sg-server/index.ts
@@ -30,8 +30,19 @@ app.set('etag', false); // turn off
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: false, limit: "5mb" }));
 
+const whitelist = ['https://shipgrid.io']
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 // Enable CORS with the above options
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Apply security headers
 app.use(helmet());
