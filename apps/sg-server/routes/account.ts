b896af3575025163898a2a3f85c8992f6a3a9878
joecof <joecofg@gmail.com>
2024-01-24 11:20:56 -0800

    remove firebase

--- apps/sg-server/routes/account.ts
+++ apps/sg-server/routes/account.ts
@@ -5,7 +5,6 @@ import {
 import {
   login,
   register,
-  firebaseLogin,
   refreshToken
 } from '../controllers/account'
 
@@ -13,7 +12,6 @@ const routes = Router()
 
 routes.post('/register', register)
 routes.post('/login', login)
-routes.post('/firebase-login', firebaseLogin)
 routes.get('/refresh-token/:firebase_token', refreshToken)
 
 export default routes
