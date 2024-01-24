b2300694d2c58c6062e924fe17d569149ed909ae
joecof <joecofg@gmail.com>
2024-01-24 11:18:39 -0800

    update

--- apps/sg-server/services/account/index.ts
+++ apps/sg-server/services/account/index.ts
@@ -1,10 +1,8 @@
 import login from './login.service'
 import register from './register.service'
-import firebaseLogin from './firebase_login.service'
 import refreshToken from './refresh_token.service'
 
 export {
-  firebaseLogin,
   login,
   register,
   refreshToken
