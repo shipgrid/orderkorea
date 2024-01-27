3071a12c22cb7c1aa056d78dbfec8a966e870971
joecof <joecofg@gmail.com>
2024-01-27 13:09:32 -0800

    update

--- apps/sg-client/src/components/Dashboard/Inventory/VehicleList.tsx
+++ apps/sg-client/src/components/Dashboard/Inventory/VehicleList.tsx
@@ -40,7 +40,7 @@ const VehicleList = ({
         <div className='request-car-content'>
           <div>
             <div style={{ fontWeight: 600, fontSize: 24, margin: '12px 0px' }}>Can't find the car you want?</div>
-            <div style={{ fontWeight: 400, fontSize: 16, margin: '12px 0px', color: '#5c5e62' }}>Access the broker network to receive the best offers that match what you are looking for</div>
+            <div style={{ fontWeight: 400, fontSize: 16, margin: '12px 0px', color: '#5c5e62' }}>Access the broker network to receive the best offers in your inbox within 48 hours </div>
             <a
               href="https://forms.gle/3q6pTTMdCXxcQK4W6"
               target="_blank"
