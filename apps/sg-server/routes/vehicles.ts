c3ec58b8001eca2d76168352d16595af19d8754e
joecof <joecofg@gmail.com>
2024-02-05 10:23:49 -0800

    fixed routing issue

--- apps/sg-server/routes/vehicles.ts
+++ apps/sg-server/routes/vehicles.ts
@@ -16,9 +16,9 @@ import authValidation from '../middlewares/auth_validation'
 const routes = Router()
 
 routes.get('/', list)
-routes.get('/:vehicle_id', get)
-routes.get('/inventory', authValidation, get_by_user_id)
 routes.post('/', authValidation, create)
+routes.get('/inventory', authValidation, get_by_user_id)
+routes.get('/:vehicle_id', get)
 routes.delete('/:vehicle_id', authValidation, remove)
 routes.put('/:vehicle_id', authValidation, update)
 
